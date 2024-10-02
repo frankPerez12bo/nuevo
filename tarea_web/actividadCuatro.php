f2b5624b3a553ccf5458e904303b8d6a89649b38
frankPerez12bo <frankperez7777pc@gmail.com>
2024-10-02 17:22:16 -0500

    Add files via upload

---
+++ tarea_web/actividadCuatro.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<!-- Recuerdad añadir la etiqueta lang -->
+<html>
+<head>
+<meta charset="UTF-8">
+<title>Ejemplo con etiquetas básicas</title>
+</head>
+<body>
+<h1 style="text-align: center;cursor: pointer;">Etiquetas HTML</h1>
+<p> Este ejemplo muestra cómo combinar algunas de las etiquetas más básicas de HTML5. <br>
+Recuerda que <strong>es importante entender la diferencias entre ellas</strong>.
+</p>
+<h2>Etiqueta ul+li</h2>
+<p>
+Si listamos personas nominadas a los Oscars, dado que el orden no altera el significado, debemos usar <em>ul</em>.
+</p>
+<ul>
+    <li>José Luis Perales</li>
+    <li>José José</li>
+    <li>Isabel Pantoja</li>
+    <li>Daniela Romo</li>
+</ul>
+<h2>Etiqueta ol+li</h2>
+<p>
+En el caso de que estemos listando elementos donde el orden es importante, como por ejemplo la clasificación de un mundial de fútbol, debemos
+usar <em>ol</em>.
+</p>
+<ol>
+    <li>Brasil</li>
+    <li>Argentina</li>
+    <li>Colombia</li>
+    <li>Chile</li>
+</ol>
+</body>
+</html>
---
+++ tarea_web/index.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html>
+<head>
+<title>(Aquí va el título de la página)</title>
+</head>
+<body>
+    <header>
+        <nav>
+            <ul>
+                <li>
+                    <a href="actividadTres.php">Actividad tres</a>
+                </li>
+                <li>
+                    <a href="actividadCuatro.php">Actividad cuatro</a>
+                </li>
+            </ul>
+        </nav>
+    </header>
+(aquí va el resto del código de html)
+</body>
+</html>
