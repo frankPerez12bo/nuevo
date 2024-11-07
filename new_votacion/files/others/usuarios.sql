9205fea82c1eab72abf27a99c60eff315997e852
frankPerez12bo <frankperez7777pc@gmail.com>
2024-11-07 15:49:15 +0100

    Add files via upload

---
+++ new_votacion/files/others/usuarios.sql
@@ -0,0 +1,77 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.1
+-- https://www.phpmyadmin.net/
+--
+-- Servidor: 127.0.0.1
+-- Tiempo de generación: 07-11-2024 a las 15:46:46
+-- Versión del servidor: 10.4.32-MariaDB
+-- Versión de PHP: 8.2.12
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Base de datos: `votacion_one`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `usuarios`
+--
+
+CREATE TABLE `usuarios` (
+  `id` int(11) NOT NULL,
+  `dni` varchar(10) NOT NULL,
+  `correo_usuario` varchar(100) NOT NULL,
+  `numero_celular` varchar(9) DEFAULT NULL,
+  `nombres` varchar(100) NOT NULL,
+  `clave_usuario` varchar(100) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Volcado de datos para la tabla `usuarios`
+--
+
+INSERT INTO `usuarios` (`id`, `dni`, `correo_usuario`, `numero_celular`, `nombres`, `clave_usuario`) VALUES
+(1, '77889912', 'camila@gmail.com', '987412357', 'camila cañihuran ayvar', 'camila1234'),
+(2, '77554433', 'gianina@gmail.com', '987412555', 'Gianina Guzman Herrera', '$2y$10$ueDpCJHlJXux.95z7tP5be6kJ8f5y.A/t7BRCYpqKl3P5GOLs4Hs2'),
+(3, '77441101', 'jose@gmail.com', '991122334', 'jose maria manuel', '$2y$10$AlKWlEZZxZEMqipCNJ5teOxU2Cp7rspNp9dVmdFSGe0X3xufDnKHO'),
+(4, '77123457', 'kevin_perezAlarcon@gmail.com', '997744112', 'kevin Jose Perez Alarcon ', 'kevin1234'),
+(5, '74195233', 'joseManuel@gmail.com', '987412333', 'Jose Manuel Ordoñes Lopez', 'jose1234'),
+(6, '74185221', 'andresSantaCruz77@gmail.com', '987412555', 'Andres Santa Cruz Ñaño', 'andres1234'),
+(8, '74123598', 'danielCruz@gmail.com', '999854111', 'Daniel Danilo Cruz Perez', 'daniel1234'),
+(10, '74189521', 'pedroAquino@gmail.com', '999414111', 'Adrian Jose Pedrin Pedrin ', 'pedro1234');
+
+--
+-- Índices para tablas volcadas
+--
+
+--
+-- Indices de la tabla `usuarios`
+--
+ALTER TABLE `usuarios`
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `dni` (`dni`);
+
+--
+-- AUTO_INCREMENT de las tablas volcadas
+--
+
+--
+-- AUTO_INCREMENT de la tabla `usuarios`
+--
+ALTER TABLE `usuarios`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=11;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
---
+++ new_votacion/files/others/votos.sql
@@ -0,0 +1,84 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.1
+-- https://www.phpmyadmin.net/
+--
+-- Servidor: 127.0.0.1
+-- Tiempo de generación: 07-11-2024 a las 15:47:16
+-- Versión del servidor: 10.4.32-MariaDB
+-- Versión de PHP: 8.2.12
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Base de datos: `votacion_one`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `votos`
+--
+
+CREATE TABLE `votos` (
+  `id` int(11) NOT NULL,
+  `usuario_id` int(11) DEFAULT NULL,
+  `candidato_id` int(11) DEFAULT NULL,
+  `fecha_votacion` timestamp NOT NULL DEFAULT current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Volcado de datos para la tabla `votos`
+--
+
+INSERT INTO `votos` (`id`, `usuario_id`, `candidato_id`, `fecha_votacion`) VALUES
+(1, 1, 2, '2024-10-31 14:52:17'),
+(2, 4, 3, '2024-10-31 14:52:17'),
+(3, 5, 2, '2024-11-07 00:22:38'),
+(4, 8, 3, '2024-11-07 01:37:13'),
+(5, 10, 3, '2024-11-07 03:28:02');
+
+--
+-- Índices para tablas volcadas
+--
+
+--
+-- Indices de la tabla `votos`
+--
+ALTER TABLE `votos`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `usuario_id` (`usuario_id`),
+  ADD KEY `candidato_id` (`candidato_id`);
+
+--
+-- AUTO_INCREMENT de las tablas volcadas
+--
+
+--
+-- AUTO_INCREMENT de la tabla `votos`
+--
+ALTER TABLE `votos`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+
+--
+-- Restricciones para tablas volcadas
+--
+
+--
+-- Filtros para la tabla `votos`
+--
+ALTER TABLE `votos`
+  ADD CONSTRAINT `votos_ibfk_1` FOREIGN KEY (`usuario_id`) REFERENCES `usuarios` (`id`),
+  ADD CONSTRAINT `votos_ibfk_2` FOREIGN KEY (`candidato_id`) REFERENCES `candidatos` (`id`);
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
